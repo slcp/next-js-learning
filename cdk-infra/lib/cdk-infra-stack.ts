8f33a8bb083640be562dd7db90393f288b2a71ea
Stuart Forrest <stuart.forrest@infinityworks.com>
2023-09-11 16:46:16 +0100

    add cloudfront distribution and foundation of sending static assets to s3

--- cdk-infra/lib/cdk-infra-stack.ts
+++ cdk-infra/lib/cdk-infra-stack.ts
@@ -1,4 +1,13 @@
 import * as cdk from "aws-cdk-lib";
+import {
+  AllowedMethods,
+  Distribution,
+  OriginProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import {
+  LoadBalancerV2Origin,
+  S3Origin,
+} from "aws-cdk-lib/aws-cloudfront-origins";
 import { Peer, Port, SecurityGroup, Vpc } from "aws-cdk-lib/aws-ec2";
 import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
 import {
@@ -8,12 +17,15 @@ import {
   LogDriver,
 } from "aws-cdk-lib/aws-ecs";
 import { ApplicationLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
+import { Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 
 export class CdkInfraStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const APP_PORT = 3000;
+    const STATIC_NEXT_PATH = "_next/static";
     const pathToDockerFile = "../front-end";
 
     const vpc = new Vpc(this, "MyVpc", {
@@ -79,5 +91,55 @@ export class CdkInfraStack extends cdk.Stack {
     scalableTarget.scaleOnCpuUtilization("cpuScaling", {
       targetUtilizationPercent: 70,
     });
+
+    const staticAssetsBucket = new Bucket(this, "StaticAssets", {
+      encryption: BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      bucketName: `next-static-assets-next-stack-sf`,
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // directory to be zipped
+   const srcCodeDir = '../front-end';
+   // target directory
+   const trgBucketCodeDir = '_next/static';
+  // zip directory, hashed name
+   const CodeAsset = Source.asset(srcCodeDir);
+
+   // Deploy as zip
+    const zipBucketDeployment = new BucketDeployment(this, 'Static Assets', {
+      sources: [CodeAsset],
+      destinationBucket: staticAssetsBucket,
+      destinationKeyPrefix: trgBucketCodeDir,
+      extract: true,
+      prune: false
+    });
+
+    // Cloudfront to sit in front of load balancer
+    const cdn = new Distribution(this, "CDN", {
+      defaultBehavior: {
+        origin: new LoadBalancerV2Origin(fargateService.loadBalancer, {
+          protocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
+        }),
+        allowedMethods: AllowedMethods.ALLOW_ALL,
+      },
+      // static assets behaviour
+      additionalBehaviors: {
+        [STATIC_NEXT_PATH]: {
+          origin: new S3Origin(staticAssetsBucket),
+        },
+      },
+    });
+
+    new cdk.CfnOutput(this, "CDNOutput", {
+      value: cdn.distributionDomainName,
+    });
+    new cdk.CfnOutput(this, "StaticAssetsBucket", {
+      value: staticAssetsBucket.bucketName,
+    });
+
+    // Support 'standalone' deployment of NextJS
+    // TODO: Ensure contents of .next/static is copied to the bucket
   }
 }
