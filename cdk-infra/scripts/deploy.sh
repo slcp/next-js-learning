4e73cd66a05c079b1c16ebb48c576ec11c0a6c03
Stuart Forrest <stuart.forrest@infinityworks.com>
2023-09-13 09:16:33 +0100

    merge two deploy scripts

--- cdk-infra/scripts/deploy.sh
+++ cdk-infra/scripts/deploy.sh
@@ -1,16 +1,49 @@
 SCRIPT_DIR=$(dirname "$0")
 LOCAL_STATIC_ASSETS_TARGET_PATH="${SCRIPT_DIR}/../out./static"
-IMAGE_SAVE_PATH="${SCRIPT_DIR}/../out/tar/next-js-image.tar"
+DOCKER_IMAGE_TAR_NAME="next-js-image.tar"
+TAR_OUT_FOLDER="${SCRIPT_DIR}/../out/tar"
+IMAGE_SAVE_PATH="${TAR_OUT_FOLDER}/${DOCKER_IMAGE_TAR_NAME}"
+FRONTEND_PATH="${SCRIPT_DIR}/../../front-end"
+HEAD_COMMIT=$(git rev-parse --short HEAD)
+TAG="next-js:${HEAD_COMMIT}"
+CONTAINER_STATIC_ASSETS_PATH="/app/.next/static"
 
-# Build the image
-CONTAINER_TAG=$($SCRIPT_DIR/docker-prepare.sh | tail -1)
-echo "Container tag is: ${CONTAINER_TAG}"
+# Build docker image
+echo "Building docker image with tag: ${TAG}..."
+docker build -t $TAG "${FRONTEND_PATH}"
+echo "Built docker image with tag: ${TAG}"
 
-# Deploy with CDK passing in the tag to deploy to ECS
-npx cdk deploy --context container_tarball="${${SCRIPT_DIR}/IMAGE_SAVE_PATH}"
+# Extract static files from container
+# Create ephemeral docker container
+echo "Creating ephemeral container from image with tag '${TAG}' to extract static assets..."
+id=$(docker create $TAG)
+echo "Created ephemeral container from image with tag '${TAG}' to extract static assets"
+
+# Copy out the .next/static folder
+echo "Extracting static assets from container..."
+docker cp $id:$CONTAINER_STATIC_ASSETS_PATH $LOCAL_STATIC_ASSETS_TARGET_PATH
+echo "Extracted static assets from container"
+
+# Save image as tarball
+echo "Saving docker image as tarball..."
+mkdir -p $TAR_OUT_FOLDER
+docker save $TAG > $IMAGE_SAVE_PATH
+echo "Saved docker image as tarball"
+echo "${IMAGE_SAVE_PATH}"
+
+# Deploy with CDK passing in the tarball path to deploy to ECS
+echo "Deploying with CDK..."
+npx cdk synth --context container_tarball="${IMAGE_SAVE_PATH}" --outputs-file out/cdk/outputs.json
+echo "Deployed with CDK"
+
+# TODO: Extract bucket name from CDK outputs using jq
+echo "Extracting bucket name from CDK outputs..."
+echo "Extracted bucket name from CDK outputs"
 
 # Copy static files to S3
+echo "Copying static assets to S3..."
 # aws s3 cp $LOCAL_STATIC_ASSETS_TARGET_PATH s3:// harcode the bucket name for now? jq to parse cdk output?
+echo "Copied static assets to S3..."
 
 # Cleanup
 rm $IMAGE_SAVE_PATH
