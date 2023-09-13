ec5dcdeac105f8a7aca0070fe6ca6c32a5261944
Stuart Forrest <stuart.forrest@infinityworks.com>
2023-09-13 09:17:06 +0100

    rename x var

--- cdk-infra/bin/cdk-infra.ts
+++ cdk-infra/bin/cdk-infra.ts
@@ -7,12 +7,12 @@ import { NextJsS3Stack } from "../lib/nextjs-s3-stack";
 const APP_PORT = 3000;
 
 const app = new cdk.App();
-const x = app.node.getContext("container_tarball");
+const containerTarballPath = app.node.getContext("container_tarball");
 
 const s3Stack = new NextJsS3Stack(app, "NextJSS3Stack", {});
 
 new NextJsAppStack(app, "NextJSAppStack", {
-  containerTarballPath: x,
+  containerTarballPath: containerTarballPath,
   staticAssetsBucketArn: s3Stack.staticAssetsBucket.bucketArn,
   appPort: APP_PORT,
 });
